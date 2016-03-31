8bb4dd38cb3a81239f8dfd2a18089d1b9f1fb271
Sergey Klimov <sergey.v.klimov@gmail.com>
2016-03-31 00:10:21 -0700

    interface

--- src/org/darvin/CatController/MainActivity.java
+++ src/org/darvin/CatController/MainActivity.java
@@ -2,8 +2,16 @@ package org.darvin.CatController;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+
+    public TextView bleStatusTextView;
+    public TextView waterStatusTextView;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +19,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        bleStatusTextView = (TextView)findViewById(R.id.bleStatusTextView);
+        waterStatusTextView = (TextView)findViewById(R.id.waterStatusTextView);
+    }
+
+
+    public void buttonTurnOnPressed(View view) {
+        Log.v(TAG, "button pressed");
+
+        waterStatusTextView.setText("Trying to turn on...");
+
+
+
     }
 }
